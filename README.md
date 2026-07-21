32f040195b89f1ab517f9a3f0a6896563c4e1b54
iceywu <3128006406@qq.com>
2026-07-21 16:22:18 +0800

    chore: update site url to tonomemo.com

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## 预览
 
-🔗 https://tono.dev
+🔗 https://tonomemo.com
 
 ## 页面
 
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tono.dev',
+  site: 'https://tonomemo.com',
   base: '/',
 });
